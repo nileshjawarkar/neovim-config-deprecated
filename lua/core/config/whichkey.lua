7d6e0358dc38e1fa4e8de79532ec5e36cdc4110d
nileshjawarkar <jawarkar.nilesh@gmail.com>
2022-05-08 21:59:01 +0530

    Modified which-key seperators.

--- lua/core/config/whichkey.lua
+++ lua/core/config/whichkey.lua
@@ -23,9 +23,9 @@ wk.setup({
 		["<tab>"] = "TAB",
 	},
 	icons = {
-		breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-		separator = "➜", -- symbol used between a key and it's label
-		group = "+", -- symbol prepended to a group
+		breadcrumb = "=", -- symbol used in the command line area that shows your active key combo
+		separator = "-> ", -- symbol used between a key and it's label
+		group = "(+) ", -- symbol prepended to a group
 	},
 	popup_mappings = {
 		scrol_down = '<c-d>', -- binding to scroll down inside the popup
