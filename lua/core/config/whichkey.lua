cd351e20dc8cfd29a95c55e857ef2a8379d92108
nileshjawarkar <jawarkar.nilesh>
2022-05-12 22:53:02 +0530

    Removed some default mappings shown by which key.

--- lua/core/config/whichkey.lua
+++ lua/core/config/whichkey.lua
@@ -1,18 +1,18 @@
 local wk = require("which-key")
 wk.setup({
 	plugins = {
-		marks = true, -- shows a list of your marks on ' and `
-		registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+		marks = false, -- shows a list of your marks on ' and `
+		registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
 		presets = {
 			operators = false, -- adds help for operators like d, y, ...
 			motions = false, -- adds help for motions
 			text_objects = false, -- help for text objects triggered after entering an operator
 			windows = false, -- default bindings on <c-w>
-			nav = true, -- misc bindings to work with windows
-			z = true, -- bindings for folds, spelling and others prefixed with z
-			g = true, -- bindings for prefixed with g
+			nav = false, -- misc bindings to work with windows
+			z = false, -- bindings for folds, spelling and others prefixed with z
+			g = false, -- bindings for prefixed with g
 		},
-		spelling = { enabled = true, suggestions = 20 }, -- use which-key for spelling hints
+		spelling = { enabled = false, suggestions = 20 }, -- use which-key for spelling hints
 	},
 	-- add operators that will trigger motion and text object completion
 	-- to enable all native operators, set the preset / operators plugin above
@@ -35,7 +35,7 @@ wk.setup({
 		border = "single", -- none, single, double, shadow
 		position = "bottom", -- bottom, top
 		margin = { 0, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]
-		padding = { 0, 0, 0, 0 }, -- extra window padding [top, right, bottom, left]
+		padding = { 1, 1, 1, 1 }, -- extra window padding [top, right, bottom, left]
 		winblend = 0
 	},
 	layout = {
